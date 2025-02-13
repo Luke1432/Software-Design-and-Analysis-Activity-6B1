6019814c81997f62ad49f701d43b16013bc35c50
Lukas Fenkam <lukas@fenkam.name>
2025-02-12 23:41:47 -0500

    #3 Completed calculator, added tests

--- test.py
+++ test.py
@@ -9,3 +9,8 @@ def mul(x,y):
 
 def div(x,y):
     return x/y
+
+print("Addition: 4+5=",add(4,5))
+print("Subtraction: 4-5=",sub(4,5))
+print("Multiplication: 4*5=",mul(4,5))
+print("Division: 4/5=",div(4,5))
