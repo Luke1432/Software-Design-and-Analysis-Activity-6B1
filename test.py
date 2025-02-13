12ab680367180fc92e12199c15441b74769f80da
Lukas Fenkam <lukas@fenkam.name>
2025-02-12 23:50:15 -0500

    #7 Added exponents and square root

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import math
+
+
 def add(x,y):
     return x+y
 
@@ -10,7 +13,15 @@ def mul(x,y):
 def div(x,y):
     return x/y
 
+def pow(x,y):
+    return x**y
+
+def sqrt(x):
+    return math.sqrt(x)
+
 print("Addition: 4+5=",add(4,5))
 print("Subtraction: 4-5=",sub(4,5))
 print("Multiplication: 4*5=",mul(4,5))
 print("Division: 4/5=",div(4,5))
+print("Exponentiation: 4^5=",pow(4,5))
+print("Square root: sqrt(4)=",sqrt(4))
